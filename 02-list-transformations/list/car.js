8d9c51e2c91f8b3886fbe7507a1823a5e1f33171
Brad Parker <hi@bradparke.com>
2016-02-19 21:11:06 +1000

    Cons with nothing but lambdas

--- 02-list-transformations/list/car.js
+++ 02-list-transformations/list/car.js
@@ -1,1 +1,2 @@
-module.exports = (cell) => cell(1)
+module.exports = (cell) =>
+  cell((a) => (b) => a)
--- 02-list-transformations/list/cons.js
+++ 02-list-transformations/list/cons.js
@@ -1,8 +1,2 @@
 module.exports = (first) => (second) =>
-  (choice) => {
-    if (choice === 1) {
-      return first
-    } else if (choice === 2) {
-      return second
-    }
-  }
+  (m) => m(first)(second)
